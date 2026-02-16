3b384c3cd6068736f3dd124824f055b9380f4c31
Gulla Naga Poojitha <gullanagapoojitha123@gmail.com>
2026-02-16 15:15:46 +0530

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,2 +1,7 @@
-Write Your Function For the Problem Statement Here
-Team name : Codepy
+# Team Name: Codepy
+
+def count_inventory(fruit_list: list[str]) -> dict[str, int]:
+    inventory = {}
+    for fruit in fruit_list:
+        inventory[fruit] = inventory.get(fruit, 0) + 1
+    return inventory
